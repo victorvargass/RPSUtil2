6316aaf7931f22b28072e9ce403672a6d9d21236
Victor Vargas <victor.vargas.sand@gmail.com>
2016-06-21 18:37:23 -0400

    Corregido comentario tonto

--- app/src/main/java/victorvs/com/rpsutil2/CursorSQLHelper.java
+++ app/src/main/java/victorvs/com/rpsutil2/CursorSQLHelper.java
@@ -30,7 +30,6 @@ public class CursorSQLHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(sqlTablaBoleta);
         db.execSQL(sqlTablaTurno);
-        //Holaaaaa
     }
 
     @Override
